5cadee28d0280f6ab9d9f7a0ccc00f1172f4ac2f
Pedro Linha <plinhares832@gmail.com>
2026-09-17 23:39:00 -0300

    Criação do projeto Colmeia Girassol

---
+++ src/test/java/br/edu/fatec/projetointerdisciplinar/ProjetoInterdisciplinarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fatec.projetointerdisciplinar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoInterdisciplinarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
